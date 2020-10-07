b5bb8bbf085786f5f487bee5cd692e49ed814779
John Jaquith <japesjake@gmail.com>
2020-10-06 23:32:44 -0400

    updated chess.py

--- chess.py
+++ chess.py
@@ -24,13 +24,14 @@ class Graphics:
         return (Board.SQUARE_SIZE * x, Board.SQUARE_SIZE * y)
 
 class Game:
+    objects = {}
     def __init__(self):
         self.running = True
-        self.objects = {}
+        # self.objects = {}
     def run(self):
         graphics = Graphics()
         board = Board()
-        self.set_square_objects()
+        self.set_squares()
         board.draw()
         self.set_pieces()
         self.draw_pieces()
@@ -39,23 +40,35 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
-    def set_square_objects(self):
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        for square, piece in self.objects.items():
+                            if piece != None: 
+                                if piece.is_clicked():
+                                    piece.selected = True
+                                    for square, piece in self.objects.items():
+                                        print(square.coords, piece)
+    def set_squares(self):
         for y_coord in range(Board.HEIGHT):
             for x_coord in range(Board.WIDTH):
                 self.objects[Square((x_coord, y_coord))] = None       
     def set_pawns(self):
         y_coord = Board.HEIGHT - 2
         for x_coord in range(Board.WIDTH):
-            self.objects[(x_coord, y_coord)] = Pawn('white')
+            for square, piece in self.objects.items():
+                if square.coords == (x_coord, y_coord):
+                    self.objects[square] = Pawn('white')
         y_coord = 1
         for x_coord in range(Board.WIDTH):
-            self.objects[(x_coord, y_coord)] = Pawn('black')
+            for square, piece in self.objects.items():
+                if square.coords == (x_coord, y_coord):
+                    self.objects[square] = Pawn('black')
     def set_pieces(self):
         self.set_pawns()
     def draw_pieces(self):
         for square in self.objects:
             if self.objects[square] != None:
-                Graphics.screen.blit(self.objects[square].image, Graphics.pixelate(square))
+                Graphics.screen.blit(self.objects[square].image, Graphics.pixelate(square.coords))
 
 
 
@@ -81,18 +94,49 @@ class Square:
     def __init__(self, coords):
         self.coords = coords
         self.highlighted = False
+    # def is_forward_square_occupied():
+    #     for square, piece in Game.objects.items():
+    #         if piece.selected:
+    #             if piece.color == 'white':
+    #                 direction = -1
+    #             else:
+    #                 direction = 1
+    #             x_selected, y_selected = square.coords
+    #             x_forward, y_forward = x_selected, y_selected + direction
+    #     for square, piece in Game.objects.items():
+    #         if square.coords == (x_forward, y_forward):
+    #             if piece != None:
+    #                 return True
+    #             return False
+        
+
 
 class Piece:
     def __init__(self):
         self.possible_moves = []
+        self.selected = False
+    # def update_possible_moves(piece):
+    #     self.possible_moves = piece.return_possible_moves()
+    def is_clicked(self):
+        mouse_x, mouse_y = pg.mouse.get_pos()
+        print(pg.mouse.get_pos())
+        for square, piece in Game.objects.items():
+            square_x, square_y = Graphics.pixelate(square.coords)
+            if square_x <= mouse_x <= square_x + Board.SQUARE_SIZE and square_y <= mouse_y <= square_y + Board.SQUARE_SIZE:
+                return True
+            return False
+
 
 class Pawn(Piece):
     def __init__(self, color):
         self.name = 'pawn'
         self.color = color
         self.image = pg.transform.scale(pg.image.load(os.path.join('pngs', self.name + '_' + self.color + '.png')), (Board.SQUARE_SIZE, Board.SQUARE_SIZE))
-    def get_possible_moves(self):
-        pass
+    def return_possible_moves(self):
+        for square in Game.objects:
+            pass
+
+
 
 def main():
     game = Game()
