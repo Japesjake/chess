af238274e29a82ad5cd3ef3771ed7de06614ebea
japesjake <japesjake@gmail.com>
2022-06-10 10:20:16 -0400

    commit before revert.

--- chess.py
+++ chess.py
@@ -595,10 +595,15 @@ class King(Piece):
         x_piece, y_piece = self.location
         #Creates factors of surrounding directions 
         factors_set = set()
-        for x in range(-1, 2):
-            for y in range(-1, 2):
-                if (x, y) != (0, 0):
-                    factors_set.add((x, y))
+        # Comments indicate direction e.g. U = Up, DL = Down left, etc.
+        factors_set.add((1, -1))  #UR
+        factors_set.add((0, -1))  #U
+        factors_set.add((-1, -1)) #UL
+        factors_set.add((-1, 0))  #L
+        factors_set.add((-1, 1))  #DL
+        factors_set.add((0, 1))   #D
+        factors_set.add((1 ,1))   #DR
+        factors_set.add((1, 0))   #R        
         def update_possible_move(factors):
             x_factor, y_factor = factors
             for square in board.squares:
